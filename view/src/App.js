84d6775553e7ba2aebaaa56f5e3dee0136e4a2c3
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2022-04-12 02:25:34 +0300

    create Header component

--- view/src/App.js
+++ view/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <ul className="navigate">
+        <li>Книги</li>
+        <li>Авторы</li>
+        <li>Жанры</li>
+      </ul>
+    </header>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
