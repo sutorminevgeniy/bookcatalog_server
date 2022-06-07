324e40e1b76b20583068194cbf238d7037c7262a
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2022-06-07 08:20:08 +0300

    made NavLink

--- view/src/components/Header.js
+++ view/src/components/Header.js
@@ -1,15 +1,33 @@
 import logo from '../logo.svg';
 import style from'./Header.module.css';
 
+import {NavLink} from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className={style.header}>
       <img src={logo} className={style.logo} alt="logo" />
       <ul className={style.navigate}>
-        <li><a href="/">О каталоге</a></li>
-        <li><a href="/books">Книги</a></li>
-        <li><a href="/authors">Авторы</a></li>
-        <li><a href="/genre">Жанры</a></li>
+        <li className={style.navItem}>
+          <NavLink 
+            to="/"
+            className={({ isActive }) => isActive ? style.active : undefined }>О каталоге</NavLink>
+        </li>
+        <li className={style.navItem}>
+          <NavLink 
+            to="/books"
+            className={({ isActive }) => isActive ? style.active : undefined }>Книги</NavLink>
+        </li>
+        <li className={style.navItem}>
+          <NavLink
+            to="/authors"
+            className={({ isActive }) => isActive ? style.active : undefined }>Авторы</NavLink>
+        </li>
+        <li className={style.navItem}>
+          <NavLink
+            to="/genre"
+            className={({ isActive }) => isActive ? style.active : undefined }>Жанры</NavLink>
+        </li>
       </ul>
     </header>
   );
