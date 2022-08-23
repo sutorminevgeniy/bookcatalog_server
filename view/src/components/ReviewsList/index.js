8c3caa9000072f89daad82c04191a351ff0967d4
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2022-08-23 13:45:13 +0300

    mappping list

--- view/src/components/ReviewsList/index.js
+++ view/src/components/ReviewsList/index.js
@@ -5,8 +5,8 @@ import Item from './Item';
 const ReviewsList = () => {
     const reviewsData = [
       { user: "Пользователь 1", message: "Отзыв о книге 1", rate: "5", id: "1" },
-      { user: "Пользователь 2", message: "Отзыв о книге 1", rate: "4", id: "2" },,
-      { user: "Пользователь 3", message: "Отзыв о книге 1", rate: "5", id: "3" },,
+      { user: "Пользователь 2", message: "Отзыв о книге 1", rate: "4", id: "2" },
+      { user: "Пользователь 3", message: "Отзыв о книге 1", rate: "5", id: "3" },
     ];
   
     const reviewsElements = reviewsData.map( 
