cdef1a26913773d6a4fe84e4c7365b3cd6d31636
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2022-08-31 15:38:10 +0300

    made store for manage state

--- view/src/index.js
+++ view/src/index.js
@@ -6,21 +6,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import state, {subscribe, addReview} from './redux/state'
+import store from './redux/state'
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 const renderEntireTree = () => {
     root.render(<React.StrictMode>
-        <App state={state} addReview={addReview} />
+        <App state={store.getState()} addReview={store.addReview.bind(store)} />
       </React.StrictMode>);
 
 };
 
 renderEntireTree();
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- view/src/redux/state.js
+++ view/src/redux/state.js
@@ -1,49 +1,58 @@
-let renderEntireTree = () => {
-    console.log('State was changed!')
-};
-
-/// Состояние на основание которого строиться приложение
-const state = {
-    booksListPage: {
-        books: [
-            { name: "Книга 1", author: "Автор 1", id: "1" },
-            { name: "Книга 2", author: "Автор 2", id: "2" },
-            { name: "Книга 3", author: "Автор 3", id: "3" },
-        ],
+// Хранилище - управляет состоянием
+const store = {
+    // Состояние на основание которого строиться приложение
+    _state: {
+        booksListPage: {
+            books: [
+                { name: "Книга 1", author: "Автор 1", id: "1" },
+                { name: "Книга 2", author: "Автор 2", id: "2" },
+                { name: "Книга 3", author: "Автор 3", id: "3" },
+            ],
+        },
+        bookPage: {
+            book: { name: "Книга 1", author: "Автор 1", id: "1" },
+            reviews: [
+                { user: "Пользователь 1", message: "Отзыв о книге 1", rate: "5", id: "1" },
+                { user: "Пользователь 2", message: "Отзыв о книге 1", rate: "4", id: "2" },
+                { user: "Пользователь 3", message: "Отзыв о книге 1", rate: "5", id: "3" },
+            ],
+        },
+        authorsListPage: {
+            authors: [
+                { author: "Автор 1", countBooks: "11", id: "1" },
+                { author: "Автор 2", countBooks: "13", id: "2" },
+                { author: "Автор 3", countBooks: "8", id: "3" },
+            ],        
+        },
     },
-    bookPage: {
-        book: { name: "Книга 1", author: "Автор 1", id: "1" },
-        reviews: [
-            { user: "Пользователь 1", message: "Отзыв о книге 1", rate: "5", id: "1" },
-            { user: "Пользователь 2", message: "Отзыв о книге 1", rate: "4", id: "2" },
-            { user: "Пользователь 3", message: "Отзыв о книге 1", rate: "5", id: "3" },
-        ],
+    getState() {
+        return this._state;
     },
-    authorsListPage: {
-        authors: [
-            { author: "Автор 1", countBooks: "11", id: "1" },
-            { author: "Автор 2", countBooks: "13", id: "2" },
-            { author: "Автор 3", countBooks: "8", id: "3" },
-        ],        
+ 
+    // Фунция обработчик по умолчанию
+    _callSubscriber() {
+        console.log('State was changed!')
     },
-};
+    // Функция которая подписывает обработчики, которые вызываются при изменение состояния
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 
-// Деиствия по измению состояния
-//  добавление отзыва в часности
-export const addReview = (message, rate) => {
-    const newReview = { 
-        user: "Пользователь Admin",
-        message: message,
-        rate: rate,
-        id: state.bookPage.reviews.length + 1 };
-    
-    state.bookPage.reviews.push(newReview);
+    // Деиствия по измению состояния
+    //  добавление отзыва в часности
+    addReview(message, rate) {
+        const newReview = { 
+            user: "Пользователь Admin",
+            message: message,
+            rate: rate,
+            id: this._state.bookPage.reviews.length + 1 };
+        
+        this._state.bookPage.reviews.push(newReview);
 
-    renderEntireTree();
-};
+        this._callSubscriber();
+    },
+}
 
-export const subscribe = (observer) => {
-    renderEntireTree = observer;
-};
+window.store = store;
 
-export default state;
+export default store;
