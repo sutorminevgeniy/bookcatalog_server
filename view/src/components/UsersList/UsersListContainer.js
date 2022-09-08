4ef03ad97c743745f69e880553421721a793330e
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2022-09-08 14:23:30 +0300

    -- remove logic to new UsersList conteiner component

--- view/src/components/UsersList/UsersListContainer.js
+++ view/src/components/UsersList/UsersListContainer.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 import {
     addUserCreator,
@@ -6,8 +8,52 @@ import {
     removeAccessCreator,
     setUsersCreator,
     setCurrentPageCreator } from '../../redux/usersList-reducer'
-
+    
 import UsersList from './UsersList'
+
+class UsersListContainer extends React.Component {
+    componentDidMount() {
+            if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+                .then(response => {
+                    this.props.setUsers(response.data.items, response.data.totalCount)
+                    // this.props.setUsers([
+                    //     { id: '1', login: 'admin', password: '',  name: 'Босс', email: '', isAdmin: true},
+                    //     { id: '2', login: 'user1', password: '',  name: 'Пользователь1', email: '', isAdmin: false},
+                    //     { id: '3', login: 'user2', password: '',  name: 'Пользователь2', email: '', isAdmin: false},
+                    // ], 3);
+                });
+        }
+    }
+
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items, response.data.totalCount)
+        });
+    }
+
+    render() {
+        let pagesCount = Math.ceil(this.props.totalUserCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
+        return (
+            <UsersList
+                users={this.props.users}
+                totalUserCount={this.props.totalUserCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                removeAccess={this.props.removeAccess}
+                giveAccess={this.props.giveAccess}
+                onPageChanged={this.onPageChanged} />
+        );        
+    }
+}
 
 
 const mapStateToProps = (state) => {
@@ -38,8 +84,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-
-const UsersListContainer = connect(mapStateToProps, mapDispatchToProps)(UsersList);
-
-
-export default UsersListContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersListContainer);
